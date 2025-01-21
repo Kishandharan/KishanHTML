cf9b621b485d78750787b41db3d9466114611001
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-21 19:15:16 +0530

    added basic structure

--- secpay-redesign/index.js
+++ secpay-redesign/index.js
@@ -1,11 +1,12 @@
 var texts = ["Text1", "Text2", "Text3", "Text4", "Text5", "Text6", "Text7"];
 
+
 class TopBar extends HTMLElement{
 	connectedCallback(){
 		this.style.backgroundColor = "darkblue";
 		this.style.padding = "10px";
 		this.style.width = "100%";
-		this.style.height = "50px";
+		this.style.height = "40px";
         this.style.position = "fixed";
 	}
 }
@@ -19,14 +20,43 @@ class SideBar extends HTMLElement{
         this.style.position = "fixed";
         for (var i = 0; i < texts.length; i++) {
         	this.innerHTML += `
-        	<div class="sidebarbuttons">
-        	`+texts[i]+
+        	<div class="sidebarbuttons" id="sidebarbuttons">
+        	`+`<p style="font-size: 30px; margin: 10px; font-family: arial; color: #e1f6ff;">${texts[i]}</p>`+
             `
             	</div>
             `	
-        }	
+        }		
+        document.getElementById("sidebarbuttons").style.marginTop = "40px";
     }
+}
+
+class Main extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML += `
+			<h1 style="font-family:arial;">Dashboard</h1>
+		`
+		this.style.position = "absolute";
+		this.style.marginLeft = "300px";
+		this.style.marginTop = "60px";
+		this.floatingCards("Main Wallet")
+	}
+
+	floatingCards(text1, text2, text3){
+		this.innerHTML += `
+			<div style="
+				box-shadow: 0px 0px 10px rgba(0,0,0,0.2); 
+				background-color: white; 
+				width: 400px; 
+				height: 100px;
+				border-radius: 15px;
+			    padding: 10px;
+			"> 
+				<p style="margin:0;">${text1}</p>
+			</div>
+		`;
+	}
 }
 
 customElements.define("top-bar", TopBar);
 customElements.define("side-bar", SideBar);
+customElements.define("main-content", Main);

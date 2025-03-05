32ce1abdca6ce6d39e5efefe816591b8336520db
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-03-05 13:35:12 +0530

    Working on chessboard

--- ChessBoard/index.js
+++ ChessBoard/index.js
@@ -1,3 +1,9 @@
+// Title and Font importing:
+var fontlink = document.createElement("link");
+document.title = "Chess Game";
+fontlink.href = "https://fonts.googleapis.com/css?family=JetBrains Mono";
+document.head.appendChild(fontlink);
+
 var board = document.createElement("table");
 board.id = "chessboard";
 	  
@@ -7,5 +13,5 @@ var set_JetBrainsMono_fontc = `
 			font-family: "JetBrains Mono"
 	    }
 	</style>
-`
+`;
 document.head.innerHTML += set_JetBrainsMono_fontc;

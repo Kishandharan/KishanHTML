25ee6789b137b4ac22aad07ceb4ea7bd451260fc
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-03-12 22:27:32 +0530

    created the icons vars

--- ChessBoard/index.js
+++ ChessBoard/index.js
@@ -4,6 +4,25 @@ document.title = "Chess Game";
 fontlink.href = "https://fonts.googleapis.com/css?family=JetBrains Mono";
 document.head.appendChild(fontlink);
 
+// Chess icons
+var chess_icons_black = {
+	king    :   "&#9818;",
+	queen   :   "&#9819;",
+	rook    :   "&#9820;",
+	bishop  :   "&#9821",
+	knight  :   "&#9822",
+	pawn    :   "&#9823"
+}
+
+var chess_icons_white = {
+	king    :   "&#9812;",
+	queen   :   "&#9813;",
+	rook    :   "&#9814;",
+	bishop  :   "&#9815",
+	knight  :   "&#9816",
+	pawn    :   "&#9817"
+}
+
 var board = document.createElement("table");
 board.id = "chessboard";
 	  

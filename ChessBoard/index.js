781ee35662df9a285e12eb38f4fa415b4a9ad6f7
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-03-13 11:21:31 +0530

    Having issues with TableData Elements

--- ChessBoard/index.js
+++ ChessBoard/index.js
@@ -1,36 +1,54 @@
-// Title and Font importing:
+// --------------- Declarations
 var fontlink = document.createElement("link");
-document.title = "Chess Game";
-fontlink.href = "https://fonts.googleapis.com/css?family=JetBrains Mono";
-document.head.appendChild(fontlink);
-
-// Chess icons
+var board = document.createElement("table");
 var chess_icons_black = {
 	king    :   "&#9818;",
 	queen   :   "&#9819;",
 	rook    :   "&#9820;",
 	bishop  :   "&#9821",
 	knight  :   "&#9822",
-	pawn    :   "&#9823"
-}
-
+	pawn    :   "&#9823"}
 var chess_icons_white = {
 	king    :   "&#9812;",
 	queen   :   "&#9813;",
 	rook    :   "&#9814;",
 	bishop  :   "&#9815",
 	knight  :   "&#9816",
-	pawn    :   "&#9817"
-}
-
-var board = document.createElement("table");
-board.id = "chessboard";
-	  
+	pawn    :   "&#9817"}	  
 var set_JetBrainsMono_fontc = `
 	<style>
 	    *{
 			font-family: "JetBrains Mono"
 	    }
-	</style>
-`;
-document.head.innerHTML += set_JetBrainsMono_fontc;
+	</style>`;
+var rows = [
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr"),
+	document.createElement("tr")]
+var tableData = [
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),
+	document.createElement("td"),]
+
+// ---------------- Settings and Main Code ----------------------
+fontlink.href = "https://fonts.googleapis.com/css?family=JetBrains Mono";
+document.title = "Chess Game";
+document.head.appendChild(fontlink);
+document.body.appendChild(board);
+for(var i = 0; i < rows.length; i++){
+	board.appendChild(rows[i]);
+
+    for(var j = 0; j < tableData.length; j++){
+		rows[i].appendChild(tableData[j]);
+	}
+}
